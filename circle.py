afc2972fe48db0e1bef8e28deaff4501863faa02
Myounghee96 <43746377+Myounghee96@users.noreply.github.com>
2019-02-21 15:42:18 +0900

    detect only yellow and green

--- circle.py
+++ circle.py
@@ -5,30 +5,40 @@ cap = cv2.VideoCapture(0)
 
 while True:
 
+    # covert video frame to HSV image
     _, frame = cap.read()
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    #bluring for eliminate noises
+    img = cv2.medianBlur(frame, 5)
+
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     # define range of color in BGR
-    lowerBlue = np.array([100, 80, 80])
-    upperBlue = np.array([140, 255, 255])
-    lowerGreen = np.array([35, 80, 80])
-    upperGreen = np.array([80, 255, 255])
-    lowerRed = np.array([-10, 100, 100])
+    lowerYellow = np.array([15, 100, 100])
+    upperYellow = np.array([30, 255, 255])
+    lowerGreen = np.array([45, 80, 80])
+    upperGreen = np.array([70, 255, 255])
+    lowerRed = np.array([-10, 150, 150])
     upperRed = np.array([10, 255, 255])
 
+
+
     # Threshold the HSV image to get three of colors
-    maskB = cv2.inRange(hsv, lowerBlue, upperBlue)
+    maskY = cv2.inRange(hsv, lowerYellow, upperYellow)
     maskG = cv2.inRange(hsv, lowerGreen, upperGreen)
     maskR = cv2.inRange(hsv, lowerRed, upperRed)
 
     # Bitwise-AND mask and original image
-    resB = cv2.bitwise_and(frame, frame, mask=maskB)
+    resY = cv2.bitwise_and(frame, frame, mask=maskY)
     resG = cv2.bitwise_and(frame, frame, mask=maskG)
     resR = cv2.bitwise_and(frame, frame, mask=maskR)
 
+
     cv2.imshow('frame',frame)
+    cv2.imshow('hsv', hsv)
     cv2.imshow('maskR',maskR)
-    cv2.imshow('blue',resB)
+    cv2.imshow('Yellow',resY)
     cv2.imshow('green', resG)
     cv2.imshow('Red', resR)
 
