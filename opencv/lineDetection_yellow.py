947a9bdabc8fe76f6455111a85baf59bca4e7f9d
Jeongha Lee <jeongha3751@kookmin.ac.kr>
2019-03-13 21:21:42 +0900

    Update lineDetection_yellow.py

--- opencv/lineDetection_yellow.py
+++ opencv/lineDetection_yellow.py
@@ -3,7 +3,7 @@
 import cv2
 import numpy as np
 
-video = cv2.VideoCapture(0)
+video = cv2.VideoCapture(2)
 
 while True:
     ret, orig_frame = video.read()
@@ -11,22 +11,48 @@ while True:
         video = cv2.VideoCapture(0)
         continue
 
+    draw_temp = orig_frame.copy()
+
     frame = cv2.GaussianBlur(orig_frame, (5, 5), 0)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     low_yellow = np.array([18, 94, 140])
     up_yellow = np.array([48, 255, 255])
     mask = cv2.inRange(hsv, low_yellow, up_yellow)
-    edges = cv2.Canny(mask, 75, 150)
+    mask = cv2.erode(mask, None, iterations=1)
+    mask = cv2.dilate(mask, None, iterations=1)
 
-    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 50, maxLineGap=50)
+    cv2.imshow("aa", mask)
+
+    edges = cv2.Canny(mask, 50, 150, apertureSize=3)
+
+    lines = cv2.HoughLines(edges, 1, np.pi / 180, 3)
+
     if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line[0]
-            cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+        for rho, theta in lines[0]:
+            print(rho,'\n', theta)
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            x1 = int(x0 + 1000 * (-b))
+            y1 = int(y0 + 1000 * (a))
+            x2 = int(x0 - 1000 * (-b))
+            y2 = int(y0 - 1000 * (a))
 
-    cv2.imshow("frame", frame)
-    cv2.imshow("edges", edges)
+        cv2.line(draw_temp, (x1, y1), (x2, y2), (255, 0, 0), 2)
+
+    cv2.imshow('edges', draw_temp)
+
+
+    # lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 50, maxLineGap=50)
+    # if lines is not None:
+    #     for line in lines:
+    #         x1, y1, x2, y2 = line[0]
+    #         cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+    #
+    # cv2.imshow("frame", frame)
+    # cv2.imshow("edges", edges)
 
     key = cv2.waitKey(25)
     if key == 27:
