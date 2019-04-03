d15d86824369836bd0d0424251fcda6ccd087249
Myounghee96 <43746377+Myounghee96@users.noreply.github.com>
2019-04-03 21:34:22 +0900

    Update circle.py

--- opencv/circle.py
+++ opencv/circle.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(2)
+CIRCLE_SIZE = 3000
 
 while True:
 
@@ -11,15 +12,14 @@ while True:
     #bluring for eliminate noises
     img = cv2.medianBlur(frame, 5)
 
-
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    # define range of color in BGR
-    lowerYellow = np.array([15, 100, 100])
+    # define range of color in HSV
+    lowerYellow = np.array([15, 90, 100])
     upperYellow = np.array([30, 255, 255])
-    lowerGreen = np.array([45, 80, 80])
-    upperGreen = np.array([70, 255, 255])
-    lowerRed = np.array([-10, 150, 150])
+    lowerGreen = np.array([55, 100, 100])
+    upperGreen = np.array([80, 255, 255])
+    lowerRed = np.array([-15, 180, 100])
     upperRed = np.array([10, 255, 255])
 
 
@@ -29,6 +29,43 @@ while True:
     maskG = cv2.inRange(hsv, lowerGreen, upperGreen)
     maskR = cv2.inRange(hsv, lowerRed, upperRed)
     # Bitwise-AND mask and original image
+
+
+    circle = frame.copy()
+
+    contours, _ = cv2.findContours(maskR, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    for contour in contours:
+        area = cv2.contourArea(contour)
+
+        if area > CIRCLE_SIZE:                 # please change area size to fit in traffic light size
+            cv2.drawContours(circle, contours, -1, (0,255,0), 3)
+            print(area)
+
+
+
+    contours, _ = cv2.findContours(maskG, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    for contour in contours:
+        area = cv2.contourArea(contour)
+
+        if area > CIRCLE_SIZE:  # please change area size to fit in traffic light size
+            cv2.drawContours(circle, contours, -1, (0, 255, 0), 3)
+
+    contours, _ = cv2.findContours(maskY, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    for contour in contours:
+        area = cv2.contourArea(contour)
+
+        if area > CIRCLE_SIZE:  # please change area size to fit in traffic light size
+            cv2.drawContours(circle, contours, -1, (0, 255, 0), 3)
+
+
+    cv2.imshow('dst', circle)
+    # print('area=', area)
+    print('len(contours)=', len(contours))
+
+
     resY = cv2.bitwise_and(frame, frame, mask=maskY)
     resG = cv2.bitwise_and(frame, frame, mask=maskG)
     resR = cv2.bitwise_and(frame, frame, mask=maskR)
@@ -42,8 +79,7 @@ while True:
         print("red")
 
     cv2.imshow('frame',frame)
-    cv2.imshow('hsv', hsv)
-    cv2.imshow('maskR',maskR)
+
     cv2.imshow('Yellow',resY)
     cv2.imshow('green', resG)
     cv2.imshow('Red', resR)
