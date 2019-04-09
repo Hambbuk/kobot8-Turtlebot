01d650ef3c92c2bd6effefe21663fa6bed0a2645
DaeYoung96 <zczczcc123@naver.com>
2019-04-09 12:49:31 +0900

    lds수정중

--- lds_test/lds_test01.py
+++ lds_test/lds_test01.py
@@ -1,1 +1,25 @@
-#test
+import rospy
+from sensor_msgs.msg import LaserScan
+import numpy as np
+global MIN_SCAN_ANGLE_RAD
+global MAX_SCAN_ANGLE_RAD
+global TURN_SPEED_MPS
+MIN_SCAN_ANGLE_RAD = (-90.0) / 180.0 * np.pi
+MAX_SCAN_ANGLE_RAD = (+90.0) / 180.0 * np.pi
+TURN_SPEED_MPS = 1.57
+rospy.Subscriber('/scan', LaserScan, callback)
+scan = LaserScan()
+#minIndex = np.ceil((MIN_SCAN_ANGLE_RAD - scan.angle_min) / scan.angle_increment)
+
+"""def callback(msg):
+ print("Value at 0 degress : " + msg.ranges[0])
+ print("Value at 90 degress : " + msg.ranges[360])
+ print("Value at 180 degress : " + msg.ranges[719])
+
+rospy.init_node('scan_values')
+sub = rospy.Subscriber('scan', LaserScan, callback)
+rospy.spin()"""
+
+print(MIN_SCAN_ANGLE_RAD)
+print(scan)
+print(scan.angle_increment)
