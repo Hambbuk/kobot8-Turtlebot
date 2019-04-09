3ae6055f6f62fddf44c256c1b0480410f92502a9
DaeYoung96 <zczczcc123@naver.com>
2019-04-10 00:50:18 +0900

    lds 센서 확인

--- lds_test/lds_test01.py
+++ lds_test/lds_test01.py
@@ -2,6 +2,7 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 import numpy as np
+
 global MIN_SCAN_ANGLE_RAD
 global MAX_SCAN_ANGLE_RAD
 global TURN_SPEED_MPS
@@ -11,7 +12,11 @@ TURN_SPEED_MPS = 1.57
 
 def callback(scan_msg):
     minIndex = np.ceil((MIN_SCAN_ANGLE_RAD - scan_msg.angle_min) / scan_msg.angle_increment)
-    print(minIndex)
+    maxIndex = np.floor((MAX_SCAN_ANGLE_RAD - scan_msg.angle_min)/ scan_msg.angle_increment)
+    midIndex = (minIndex+maxIndex)/2
+    #print(maxIndex)
+    closestRange_left=scan_msg.ranges[int(minIndex)]
+    print(closestRange_left)
 
 rospy.init_node('scan_values')
 sub = rospy.Subscriber('scan', LaserScan, callback)
