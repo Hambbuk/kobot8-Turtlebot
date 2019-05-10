1bc610f2ca096860a6b09ca52886f0acd0411f0a
DaeYoung96 <zczczcc123@naver.com>
2019-05-10 19:19:49 +0900

    lds update (Not IMU)

--- lds_test/tunnel.py
+++ lds_test/tunnel.py
@@ -2,10 +2,7 @@ import rospy
 import tf
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
-from sensor_msgs.msg import Imu
-from geometry_msgs.msg import Twist
 import numpy as np
-import math
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
 
@@ -59,7 +56,7 @@ def avge(arr):
             continue
         else:
             l.append(i)
-    return sum(l)/(len(l)+0.01)
+    return sum(l)/len(l)
 
 #################################################################################################################################
 #check left wall
@@ -95,30 +92,11 @@ def scan_Front_Wall(arr):
 
 #################################################################################################################################
 #Subscriber odom
-def call_test(imu):
+def call_test(odom):
     r_late = rospy.Rate(10)
-    #list = [imu.orientation.x, odom.pose.pose.orientation.y, odom.pose.pose.orientation.z, odom.pose.pose.orientation.w]
-    x = imu.orientation.x
-    y = imu.orientation.y
-    z = imu.orientation.z
-    w = imu.orientation.w
-
-    ysqr = y * y
-	
-    t0 = +2.0 * (w * x + y * z)
-    t1 = +1.0 - 2.0 * (x * x + ysqr)
-    X = math.degrees(math.atan2(t0, t1))
-	
-    t2 = +2.0 * (w * y - z * x)
-    t2 = +1.0 if t2 > +1.0 else t2
-    t2 = -1.0 if t2 < -1.0 else t2
-    Y = math.degrees(math.asin(t2))
-
-    t3 = +2.0 * (w * z + x * y)
-    t4 = +1.0 - 2.0 * (ysqr + z * z)
-    Z = math.degrees(math.atan2(t3, t4))
-    #theta = euler_from_quaternion(list)
-    print(X)
+    list = [odom.pose.pose.orientation.x, odom.pose.pose.orientation.y, odom.pose.pose.orientation.z, odom.pose.pose.orientation.w]
+    theta = euler_from_quaternion(list)
+    print(theta[2])
     r_late.sleep()
 
 #store_pose
@@ -177,8 +155,7 @@ def destination5(data1):
 
 def recive():
     rospy.Subscriber('/scan', LaserScan, callback)
-    #rospy.Subscriber('/odom', Odometry, call_test)
-    rospy.Subscriber('/imu', Imu, call_test)
+    rospy.Subscriber('/odom', Odometry, call_test)
     rospy.spin()
 
 if __name__ == '__main__':
