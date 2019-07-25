341062c90bb833b1dbf1e260a84263e1c4a564f4
DaeYoung96 <zczczcc123@naver.com>
2019-07-24 19:34:11 -0700

    db연동 - 어떻게 설계할지 생각해봐야함

--- EBSW/kobot/src/chatter.py
+++ EBSW/kobot/src/chatter.py
@@ -11,8 +11,19 @@ firebase_admin.initialize_app(cred,{
     'databaseURL' : 'https://ebsw-283e9.firebaseio.com/'
 })
 
-ref = db.reference() #
+
+#기본위치
+sec_0 = db.reference('sector_0') #
+sec_1 = db.reference('sector_1') #
+sec_2 = db.reference('sector_2') #
+sec_3 = db.reference('sector_3') #
+
 #ref.update({'test' : 'chk'}) #
 
-print(ref.get()) #
-print("test")
+print(sec_0.get())
+sec_0_x, sec_0_y = sec_0.get().split()
+sec_1_x, sec_1_y = sec_1.get().split()
+sec_2_x, sec_2_y = sec_2.get().split()
+sec_3_x, sec_3_y = sec_3.get().split()
+
+
