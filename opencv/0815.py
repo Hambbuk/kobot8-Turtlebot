c30cc41ea7c7e3a61c1eda364eb82799277157ae
DaeYoung96 <zczczcc123@naver.com>
2019-08-15 21:59:54 +0900

    ho u

--- opencv/0815.py
+++ opencv/0815.py
@@ -1,9 +1,24 @@
 import cv2
 import numpy as np
+import rospy
+from geometry_msgs.msg import Twist
 
-video = cv2.VideoCapture(2)
-w_list = []
-y_list = []
+pub = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
+
+lastError = 0
+MAX_VEL = 0.12
+
+def t_move(linear, angular):
+    twist = Twist()
+    twist.linear.x = linear
+    twist.angular.z = angular
+    pub.publish(twist)
+
+video = cv2.VideoCapture(-1)
+rospy.init_node('line_test')
+
+f_list = [(-1, -1)]
+s_list = [(999, 999)]
 
 while True:
     ret, orig_frame = video.read()
@@ -13,11 +28,11 @@ while True:
 
     draw_temp = orig_frame.copy()
 
-    cuttingImg = draw_temp[250:, :]
+    cuttingImg = draw_temp[350:, :]
 
     draw_temp = cuttingImg
 
-    M = np.ones(draw_temp.shape, dtype="uint8") * 90
+    M = np.ones(draw_temp.shape, dtype="uint8") * 30
     draw_temp = cv2.subtract(draw_temp, M)
 
     frame = cv2.GaussianBlur(draw_temp, (5, 5), 0)
@@ -30,20 +45,14 @@ while True:
 
     lines = cv2.HoughLinesP(edges, 1, np.pi / 360, 100, 100, maxLineGap=50)
 
-    f_list = []
     if lines is not None:
-        y_list.clear()
+	del f_list[:]
         for line in lines:
             x1, y1, x2, y2 = line[0]
             f_list.append((x1,y1))
             f_list.append((x2,y2))
-
-            y_list.append([(x1, y1),(x2, y2)])
         cv2.circle(frame, max(f_list), 10, (0,0,255), -1)
         cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 4)
-    else:
-        print(y_list[-1][0])
-        print(y_list[-1][1])
 
 
     lower_white = np.array([0, 0, 150])
@@ -52,31 +61,43 @@ while True:
     mask = cv2.inRange(hsv, lower_white, upper_white)
     edges = cv2.Canny(mask, 75, 150)
 
-    s_list = []
     lines = cv2.HoughLinesP(edges, 1, np.pi / 360, 100,100, maxLineGap=50)
     if lines is not None:
-        w_list.clear()
+	del s_list[:]
         for line in lines:
             x1, y1, x2, y2 = line[0]
             s_list.append((x1,y1))
             s_list.append((x2,y2))
 
-            w_list.append([(x1, y1), (x2, y2)])
-
-        (x,y) = min(s_list)
         cv2.circle(frame, min(s_list), 10, (255,255,0), -1)
         cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 4)
-    else:
-        print(w_list[-1][0])
-        print(w_list[-1][1])
-
 
 
     # cv2.line(frame, ((w_x1+y_x1)//2, (w_y1+y_y1)//2),((w_x2+y_x2)//2,(y_y2+w_y2)//2),(0, 255, 255), 4)
+    (x1, y1) = min(s_list)
+    (x2, y2) = max(f_list)
+    ave = (x1 + x2) / 2
+    cv2.circle(frame, ((x1+x2)//2,(y1+y2)//2) , 10, (200,200,25), -1)
+    #print("s_list : ", min(s_list))
+    error = ave-315
+    #print(error, " error")
+    Kp = 0.004
+    Kd = 0.025
+    print(error)
+    angular_z = Kp * error + Kd * (error - lastError)
+    #print(angular_z, "is angular_z")
+    lastError = error
+    linear_x = min(MAX_VEL * ((1 - abs(error) / 500) ** 2.2), 0.2)
+    #print(linear_x, " is linear_x")
+    angular_z = -min(angular_z, 2.0) if angular_z<0 else -max(angular_z, -2.0)
+    #print(angular_z, " is twist angular_z")
+    
+    t_move(linear_x, angular_z)
     cv2.imshow("frame", frame)
 
     key = cv2.waitKey(25)
     if key == 27:
+	t_move(0,0)
         break
 
 video.release()
