c8447bf40f0fc1324e7d04566e1313331518027b
jjolbo <jeongha3751@kookmin.ac.kr>
2019-08-16 21:34:13 +0900

    Modify HoughCircles param2

--- jjolbo-turtlebot/arrows_sign.py
+++ jjolbo-turtlebot/arrows_sign.py
@@ -2,16 +2,13 @@ import cv2
 import numpy as np
 from scipy.stats import itemfreq
 
-
 def get_dominant_color(image, n_colors):
     pixels = np.float32(image).reshape((-1, 3))
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 200, .1)
     flags = cv2.KMEANS_RANDOM_CENTERS
-    flags, labels, centroids = cv2.kmeans(
-        pixels, n_colors, None, criteria, 10, flags)
+    flags, labels, centroids = cv2.kmeans(pixels, n_colors, None, criteria, 10, flags)
     palette = np.uint8(centroids)
     return palette[np.argmax(itemfreq(labels)[:, -1])]
-
 
 clicked = False
 def onMouse(event, x, y, flags, param):
@@ -19,17 +16,12 @@ def onMouse(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONUP:
         clicked = True
 
-
 if __name__ == '__main__':
     cameraCapture = cv2.VideoCapture(2)
     cv2.namedWindow('camera')
     cv2.setMouseCallback('camera', onMouse)
 
-    # Read and process frames in loop
     success, frame = cameraCapture.read()
-
-
-
 
     while success and not clicked:
         cv2.waitKey(1)
@@ -37,8 +29,7 @@ if __name__ == '__main__':
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         img = cv2.medianBlur(gray, 37)
-        circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT,
-                                  1, 50, param1=120, param2=40)
+        circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 50, param1=120, param2=55)
 
         if not circles is None:
             circles = np.uint16(np.around(circles))
@@ -53,25 +44,21 @@ if __name__ == '__main__':
 
                 dominant_color = get_dominant_color(square, 2)
                 if dominant_color[2] > 100:
-                    # print("STOP")
                     pass
                 elif dominant_color[0] > 80:
                     zone_0 = square[square.shape[0]*3//10:square.shape[0]
                                     * 5//10, square.shape[1]*1//8:square.shape[1]*3//8]
                     cv2.imshow('Zone0', zone_0)
                     zone_0_color = get_dominant_color(zone_0, 1)
-                    # print(sum(zone_0_color))
 
                     zone_1 = square[square.shape[0]*1//8:square.shape[0]
                                     * 3//8, square.shape[1]*3//8:square.shape[1]*5//8]
-                    # cv2.imshow('Zone1', zone_1)
                     zone_1_color = get_dominant_color(zone_1, 1)
 
                     zone_2 = square[square.shape[0]*2//8:square.shape[0]
                                     * 5//8, square.shape[1]*5//8:square.shape[1]*7//8]
                     cv2.imshow('Zone2', zone_2)
                     zone_2_color = get_dominant_color(zone_2, 1)
-                    # print(sum(zone_2_color))
 
                     if zone_1_color[2] < 60:
                         if sum(zone_0_color) > sum(zone_2_color):
@@ -79,8 +66,6 @@ if __name__ == '__main__':
                         else:
                             print("RIGHT")
                     else:
-                        # if sum(zone_1_color) > sum(zone_0_color) and sum(zone_1_color) > sum(zone_2_color):
-                        #     print("FORWARD")
                         if sum(zone_0_color) > sum(zone_2_color):
                             print("LEFT")
                         else:
@@ -92,7 +77,6 @@ if __name__ == '__main__':
                 cv2.circle(frame, (i[0], i[1]), i[2], (0, 255, 0), 2)
                 cv2.circle(frame, (i[0], i[1]), 2, (0, 0, 255), 3)
         cv2.imshow('camera', frame)
-
 
     cv2.destroyAllWindows()
     cameraCapture.release()
