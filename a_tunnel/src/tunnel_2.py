0b6e077f060e7f3cad46f269299589ff67d2baac
DaeYoung96 <zczczcc123@naver.com>
2019-08-19 18:35:07 +0900

    tunnel finish

--- a_tunnel/src/tunnel_2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import rospy
-import numpy
-from std_msgs.msg import Float32
-from sensor_msgs.msg import LaserScan
-from std_msgs.msg import Int8
-from a_tunnel.msg import Data
-import copy
